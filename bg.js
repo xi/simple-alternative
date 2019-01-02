ef8b4a9c2d913c10422e19994fa20a2a3be4048f
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-01-02 14:12:33 +0100

    switch to browser action

--- bg.js
+++ bg.js
@@ -1,13 +1,14 @@
 (function() {
 	"use strict";
+	chrome.browserAction.disable();
 
 	chrome.runtime.onMessage.addListener(function(request, sender) {
 		if (request === 'showPageAction' && sender) {
-			chrome.pageAction.show(sender.tab.id);
+			chrome.browserAction.enable(sender.tab.id);
 		}
 	});
 
-	chrome.pageAction.onClicked.addListener(function(tab) {
+	chrome.browserAction.onClicked.addListener(function(tab) {
 		chrome.tabs.sendMessage(tab.id, 'simplify');
 	});
 })();
